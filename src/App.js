df5b2975b247a3245ca3b68972a860cb7e6c6607
Zamirabei <bellofachoz@gmail.com>
2024-05-25 01:36:27 +0600

    домашнее задание №5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, {useLayoutEffect, useState} from 'react';
-import MainPage from './components/mainPage/MainPage'
-import ErrorPage from './components/errorPage/ErrorPage'
+import FormList from './components/mainList/FormList'
+
 const App = () => {
-    const [paper, setPaper] = useState({
-        name: '',
-        lastname: ''
-    })
-    useLayoutEffect(() => {
-        const lookName = prompt('Пожалуйста, введите ваше имя')
-        const lookSurname = prompt('Пожалуйста введите фамилию')
-        if (lookName !== null && lookSurname !== null){
-            setPaper({name: lookName, lastname: lookSurname})
-        }
-    }, []);
-    if(paper.name === 'John'&& paper.lastname === 'Johns'){
-        return <MainPage user={paper}/>
-    }
-    else {
-        return <ErrorPage user={paper}/>
-    }
+ return (
+     <div>
+      <FormList/>
+     </div>
+ )
 };
 
 export default App;
